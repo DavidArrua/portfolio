b5d509a084d821b14c40b1822af1f26aeb3762c9
unknown <dav.arrua02@gmail.com>
2022-10-24 23:16:58 -0300

    se agrego data de certificaciones

--- js/main.js
+++ js/main.js
@@ -33,6 +33,24 @@ const app = Vue.
             'technologies' : ["JavaScript", "Vue.js", "CSS", "Boostrap 5", "HTML", "Axios"],
             'description' : "aasdfasdfadf"
           }
+        ],
+        certifications : [
+          {
+            'name': "Java",
+            'URL' : "https://community.open-bootcamp.com/user/davidarrua/certificaciones/edb2f026-f1f8-4657-996b-64ae80524980"
+          },
+          {
+            'name': "JavaScript",
+            'URL' : "https://community.open-bootcamp.com/user/davidarrua/certificaciones/fb244d9b-f6cc-47ea-8e57-d807fdd1e675"
+          },
+          {
+            'name': "MindHub",
+            'URL' : "https://www.credly.com/badges/d8eb0d6d-b63f-4d44-ab9e-63ee68f3f9b8/public_url"
+          },
+          {
+            'name': "EDteam",
+            'URL' : "https://app.ed.team/u/davidarrua/curso/programacion"
+          },
         ]
       }
     },
